2bcd2357745fc9e415ea93edca3c93868d617138
anusha-l23 <anusha.l@leanfolks.com>
2023-12-28 00:10:50 +0530

    added signup api

--- src/components/common/SignUpForm.jsx
+++ src/components/common/SignUpForm.jsx
@@ -1,8 +1,66 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+import axios from "axios";
 
 const SignUpForm = () => {
+const navigate = useNavigate();
+  const initialValues = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    optCheck:false,
+  };
+
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string().required('First Name is required'),
+    lastName: Yup.string().required('Last Name is required'),
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Passwords must match')
+      .required('Confirm Password is required'),
+      optCheck: Yup.boolean().required('This field is required'),
+  });
+
+  const onSubmit = async (values, { setSubmitting }) => {
+    try {
+      const response = await axios.post('http://localhost:3001/santarun/signup', 
+        values,
+        {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }
+      );
+
+      if (response.ok) {
+    
+        console.log('Signup successful!');
+        navigate("/login");
+      } else {
+  
+        const errorData = await response.json();
+        console.error('Signup failed:', errorData);
+      }
+    } catch (error) {
+      console.error('Error during signup:', error);
+    }
+
+    setSubmitting(false);
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
+
   return (
-    <form className="row y-gap-20">
+    <form className="row y-gap-20" onSubmit={formik.handleSubmit}>
       <div className="col-12">
         <h1 className="text-22 fw-500">Welcome back</h1>
         <p className="mt-10">
@@ -16,7 +74,18 @@ const SignUpForm = () => {
 
       <div className="col-12">
         <div className="form-input ">
-          <input type="text" required />
+        <input
+          id="firstName"
+          name="firstName"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.firstName}
+          required
+        />
+        {formik.touched.firstName && formik.errors.firstName && (
+          <div>{formik.errors.firstName}</div>
+        )}
           <label className="lh-1 text-14 text-light-1">First Name</label>
         </div>
       </div>
@@ -24,7 +93,18 @@ const SignUpForm = () => {
 
       <div className="col-12">
         <div className="form-input ">
-          <input type="text" required />
+        <input
+          id="lastName"
+          name="lastName"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.lastName}
+          required
+        />
+        {formik.touched.lastName && formik.errors.lastName && (
+          <div>{formik.errors.lastName}</div>
+        )}
           <label className="lh-1 text-14 text-light-1">Last Name</label>
         </div>
       </div>
@@ -32,7 +112,18 @@ const SignUpForm = () => {
 
       <div className="col-12">
         <div className="form-input ">
-          <input type="text" required />
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+          required
+        />
+        {formik.touched.email && formik.errors.email && (
+          <div>{formik.errors.email}</div>
+        )}
           <label className="lh-1 text-14 text-light-1">Email</label>
         </div>
       </div>
@@ -40,7 +131,18 @@ const SignUpForm = () => {
 
       <div className="col-12">
         <div className="form-input ">
-          <input type="password" required />
+        <input
+          id="password"
+          name="password"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.password}
+          required
+        />
+        {formik.touched.password && formik.errors.password && (
+          <div>{formik.errors.password}</div>
+        )}
           <label className="lh-1 text-14 text-light-1">Password</label>
         </div>
       </div>
@@ -48,7 +150,18 @@ const SignUpForm = () => {
 
       <div className="col-12">
         <div className="form-input ">
-          <input type="password" required />
+        <input
+          id="confirmPassword"
+          name="confirmPassword"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.confirmPassword}
+          required
+        />
+        {formik.touched.confirmPassword && formik.errors.confirmPassword && (
+          <div>{formik.errors.confirmPassword}</div>
+        )}
           <label className="lh-1 text-14 text-light-1">Confirm Password</label>
         </div>
       </div>
@@ -57,7 +170,16 @@ const SignUpForm = () => {
       <div className="col-12">
         <div className="d-flex ">
           <div className="form-checkbox mt-5">
-            <input type="checkbox" name="name" />
+            <input type="checkbox" 
+            name="optCheck"
+            checked={formik.values.optCheck}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          required
+        />
+        {formik.touched.optCheck && formik.errors.optCheck && (
+          <div>{formik.errors.optCheck}</div>
+        )}
             <div className="form-checkbox__mark">
               <div className="form-checkbox__icon icon-check" />
             </div>
--- src/components/hotel-list/hotel-list-v5/MainFilterSearchBox1.jsx
+++ src/components/hotel-list/hotel-list-v5/MainFilterSearchBox1.jsx
@@ -1,17 +1,13 @@
 //import DateSearch from "../common/DateSearch";
-import LocationSearch from "./LocationSearch";
+//import LocationSearch from "./LocationSearch";
 //import GuestSearch from "../common/GuestSearch";
 import EventSearch from "./EventSearch";
 import BibName from "./BibName";
-import Year from "./Year";
 const MainFilterSearchBox1 = () => {
   return (
     <>
       <div className="mainSearch -col-3-big bg-white px-10 py-10 lg:px-20 lg:pt-5 lg:pb-20 rounded-4 mt-30">
         <div className="button-grid items-center">
-        <div className="searchMenu-date px-30 lg:py-20  sm:px-20 js-form-dd js-calendar">
-          <Year />
-</div>
           <div className="searchMenu-date px-30 lg:py-20  sm:px-20 js-form-dd js-calendar">
             {/* <div>
               <h4 className="text-15 fw-500 ls-2 lh-16">
